94986c1a09e9043eb77f40089d18bff095c47f0d
Hannes Spitz <brokkoli71b@gmail.com>
2026-04-23 22:46:35 +0200

    task2 works for non power of 2

--- assignments/03_assignment/src/task2.py
+++ assignments/03_assignment/src/task2.py
@@ -7,26 +7,25 @@ import torch
 import triton
 
 def main():
-    M = 32
-    N = 32
-    K = 32
-    # TODO: use this padded sizes
-    M_padded = int(2**math.ceil(math.log2(M))) 
-    N_padded = int(2**math.ceil(math.log2(N)))
-    K_padded = int(2**math.ceil(math.log2(K)))
-    m_tile=4
-    n_tile=4
-    k_tile=4
+    M = 321
+    N = 123
+    K = 23
+    m_tile=64
+    n_tile=32
+    k_tile=128
+
+    M_padded = int(2**math.ceil(math.log2(max(M, m_tile)))) 
+    N_padded = int(2**math.ceil(math.log2(max(N, n_tile))))
+    K_padded = int(2**math.ceil(math.log2(max(K, k_tile))))
 
 
     A = torch.randn((M, K), device='cuda', dtype=torch.float16)
     B = torch.randn((K, N), device='cuda', dtype=torch.float16)
     C = torch.empty((M, N), device='cuda', dtype=torch.float32)
 
-    grid = ((M_padded // m_tile) * (N_padded // n_tile), )
-
+    grid = (math.ceil(M_padded / m_tile) * math.ceil(N_padded / n_tile), )
     torch.cuda.init()
-    ct.launch(torch.cuda.current_stream(), grid, kernel_fp16, (A, B, C, m_tile, n_tile, k_tile))
+    ct.launch(torch.cuda.current_stream(), grid, kernel_fp16, (A, B, C, m_tile, n_tile, k_tile, M_padded, N_padded, K_padded))
     torch.cuda.synchronize()
 
     expected = torch.empty((M, N), device='cuda', dtype=torch.float16)
@@ -38,13 +37,12 @@ def main():
 
 
 @ct.kernel
-def kernel_fp16(A, B, C, m_tile: ct.Constant[int], n_tile: ct.Constant[int], k_tile: ct.Constant[int]):
+def kernel_fp16(A, B, C, m_tile: ct.Constant[int], n_tile: ct.Constant[int], k_tile: ct.Constant[int], M_padded: ct.Constant[int], N_padded: ct.Constant[int], K_padded: ct.Constant[int]):
     bid = ct.bid(0)
-    bid_x = bid % (C.shape[1] // n_tile)
-    bid_y = bid // (C.shape[1] // n_tile)
-
+    bid_x = bid % (N_padded // n_tile)
+    bid_y = bid // (N_padded // n_tile)
     result = ct.zeros((m_tile, n_tile), dtype=torch.float32)
-    for k in range(0, A.shape[1] // k_tile):
+    for k in range(0, K_padded // k_tile):
         A_block = ct.load(A, index=(bid_y , k), shape=(m_tile, k_tile), padding_mode=ct.PaddingMode.ZERO)
         B_block = ct.load(B, index=(k, bid_x), shape=(k_tile, n_tile), padding_mode=ct.PaddingMode.ZERO)
         result = ct.mma(A_block, B_block, acc=result)
