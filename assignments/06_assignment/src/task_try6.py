789b01074e5cb78fb7f6f10c8174824be52c6d90
Falko Linke <falkolinke@gmail.com>
2026-05-19 00:26:25 +0200

    Add new scripts for tensor contraction and benchmarking with optimized Triton kernel

--- assignments/06_assignment/src/task_try6.py
+++ assignments/06_assignment/src/task_try6.py
@@ -17,50 +17,54 @@ sys.path.append(str(assignment_05_src))
 from optimizer import Optimizer
 from config import Config, DataType, PrimType, DimType, ExecType, generate_config
 
-# Config(
-#     data_type=DataType.FLOAT16,
-#     prim_main=PrimType.GEMM,
-#     prim_last=LastType.NONE,
-#     prim_first=FirstType.ZERO,
-#     dim_types=[<DimType.M: 0>, <DimType.M: 0>, <DimType.N: 1>, <DimType.M: 0>, <DimType.N: 1>, <DimType.K: 2>, <DimType.M: 0>, <DimType.N: 1>],
-#     exec_types=[<ExecType.PAR: 1>, <ExecType.PAR: 1>, <ExecType.PAR: 1>, <ExecType.PAR: 1>, <ExecType.PAR: 1>, <ExecType.PRIM: 2>, <ExecType.PRIM: 2>, <ExecType.PRIM: 2>],
-#     dim_sizes=[12, 6, 18, 4, 4, 4096, 64, 64],
-#     strides=[[6291456, 256, 0, 64, 0, 1536, 1, 0], [0, 0, 256, 0, 64, 4608, 0, 1], [7077888, 1179648, 256, 294912, 64, 0, 4608, 1]]
-# )
 @ct.kernel
-def contraction(A, B, C, m1: ct.Constant[int], n1: ct.Constant[int], k: ct.Constant[int], n0: ct.Constant[int], m0: ct.Constant[int]):
-    m3_i = ct.bid(0)
-    m2_i = ct.bid(1)
+def contraction(A, B, C, 
+                m5: ct.Constant[int], m4: ct.Constant[int], 
+                n3: ct.Constant[int], m2: ct.Constant[int], n2: ct.Constant[int], 
+                m1: ct.Constant[int], n1: ct.Constant[int], 
+                m0: ct.Constant[int], k: ct.Constant[int], n0: ct.Constant[int]):
 
-    bc_it = ct.bid(2)
-    n1_i = bc_it % n1
-    temp = bc_it // n1
-    m1_i = temp % m1
-    n2_i = temp // m1
-    
+    # 1. Block ID 0: m5 * m4
+    bid_0 = ct.bid(0)
+    m4_i = bid_0 % m4
+    m5_i = bid_0 // m4
+
+    # 2. Block ID 1: n3 * m2 * n2
+    bid_1 = ct.bid(1)
+    n2_i = bid_1 % n2
+    temp1 = bid_1 // n2
+    m2_i = temp1 % m2
+    n3_i = temp1 // m2
+
+    # 3. Block ID 2: m1 * n1
+    bid_2 = ct.bid(2)
+    n1_i = bid_2 % n1
+    m1_i = bid_2 // n1
+
     acc = ct.zeros((m0, n0), dtype=ct.float32)
+
+    k_t = 64
     
-    for k_i in range(0, k, 64):
+    for k_i in range(0, k, k_t):
         A_ = ct.load(
             A, 
-            index=(m3_i, m2_i, n2_i, m1_i, n1_i, k_i, 0, 0), 
-            shape=(1,1,1,1,1,64,m0,1), 
+            index=(m5_i, m4_i, n3_i, m2_i, n2_i, m1_i, n1_i, 0, k_i, 0), 
+            shape=(1, 1, 1, 1, 1, 1, 1, m0, k_t, 1), 
             padding_mode=ct.PaddingMode.ZERO
         )
-        A_ = ct.reshape(A_, (64, m0))
-        A_ = ct.transpose(A_)
+        A_ = ct.reshape(A_, (m0, k_t))
         B_ = ct.load(
             B, 
-            index=(m3_i, m2_i, n2_i, m1_i, n1_i, k_i, 0, 0), 
-            shape=(1,1,1,1,1,64,1,n0), 
+            index=(m5_i, m4_i, n3_i, m2_i, n2_i, m1_i, n1_i, 0, k_i, 0), 
+            shape=(1, 1, 1, 1, 1, 1, 1, 1, k_t, n0), 
             padding_mode=ct.PaddingMode.ZERO
         )
-        B_ = ct.reshape(B_, (64, n0))
+        B_ = ct.reshape(B_, (k_t, n0))
         acc += ct.matmul(A_, B_)
 
     acc = ct.astype(acc, ct.float16)
-    acc = ct.reshape(acc, (1,1,1,1,1,1,m0,n0))
-    ct.store(C, index=(m3_i, m2_i, n2_i, m1_i, n1_i, 0, 0, 0), tile=acc)
+    acc = ct.reshape(acc, (1, 1, 1, 1, 1, 1, 1, m0, 1, n0))
+    ct.store(C, index=(m5_i, m4_i, n3_i, m2_i, n2_i, m1_i, n1_i, 0, 0, 0), tile=acc)
 
 
 
@@ -75,7 +79,6 @@ if __name__ == "__main__":
 
     # Convert all tensors to torch tensors and move them to the GPU before calling `torch.einsum`. Run the contraction **twice**: once with `torch.float32` inputs and once with `torch.float16` inputs (cast the tensors before contracting).
     einsum_string = 'acspx,bspy->abcyx'
-    # M = acx  N = by  K = sp   C =
 
     tensor_acspx_32 = tensor_acspx.to('cuda')
     tensor_bspy_32 = tensor_bspy.to('cuda')
@@ -86,24 +89,19 @@ if __name__ == "__main__":
     config = generate_config(einsum_string, [tensor_acspx_16.shape, tensor_bspy_16.shape], dim_order=None)
     file_dir = Path(__file__).parent
 
-    #print(config);
-    # with open(file_dir / "results" / "task2_config.out", "w") as f:
-    #     f.write(str(config))
 
     opti = Optimizer(config)
     print(opti.config)
-    opti.fuse_dims(5,6)
     opti.fuse_dims(2,3)
-    opti.fuse_dims(0,1)
     print(opti.make_executable())
-    opti.split_dim(1, outer_size=None, inner_size=64)
+    opti.split_dim(4, outer_size=None, inner_size=64)
     print(opti.make_executable())
-    opti.split_dim(3, outer_size=None, inner_size=64)
-    print(opti.make_executable())
-    opti.split_dim(1, outer_size=None, inner_size=4)
-    opti.split_dim(3, outer_size=None, inner_size=4)
-    print(opti.make_executable())
-    opti.permute_dims([0, 1, 3, 2, 4, 7, 5, 6])
+    opti.split_dim(4, outer_size=None, inner_size=64)
+    opti.make_executable()
+    opti.split_dim(4, outer_size=None, inner_size=6)
+    opti.split_dim(3, outer_size=None, inner_size=6)
+    opti.make_executable()
+    opti.permute_dims([0, 1, 2, 3, 5, 4, 6, 7, 9, 8])
     
     print(opti.config)
 
@@ -136,25 +134,32 @@ if __name__ == "__main__":
         stride=opti.config.strides[2]
     )
 
-    grid = (opti.config.dim_sizes[0], opti.config.dim_sizes[1], opti.config.dim_sizes[2] * opti.config.dim_sizes[3] * opti.config.dim_sizes[4])
-    
+    grid = (
+        opti.config.dim_sizes[0] * opti.config.dim_sizes[1],                           # bid(0): m5 und m4
+        opti.config.dim_sizes[2] * opti.config.dim_sizes[3] * opti.config.dim_sizes[4], # bid(1): n3, m2, n2
+        opti.config.dim_sizes[5] * opti.config.dim_sizes[6]                            # bid(2): m1, n1
+    )
 
     ct.launch(
         torch.cuda.current_stream(), 
         grid, 
         contraction, 
-        (A_opt, B_opt, C_opt, opti.config.dim_sizes[3], opti.config.dim_sizes[4], opti.config.dim_sizes[5], opti.config.dim_sizes[6], opti.config.dim_sizes[7])
+        (A_opt, B_opt, C_opt, 
+         opti.config.dim_sizes[0], opti.config.dim_sizes[1], opti.config.dim_sizes[2], 
+         opti.config.dim_sizes[3], opti.config.dim_sizes[4], opti.config.dim_sizes[5], 
+         opti.config.dim_sizes[6], opti.config.dim_sizes[7], opti.config.dim_sizes[8], 
+         opti.config.dim_sizes[9])
     )
 
     C_final = C_acxby.permute(0, 3, 1, 4, 2)
 
     expected = torch.einsum(einsum_string, tensor_acspx_16, tensor_bspy_16)
-    assert torch.allclose(C_final, expected, atol=1.5e-0), "The result is incorrect!"
+    assert torch.allclose(C_final, expected, atol=2e-0), "The result is incorrect!"
     print("The result is correct!")
 
     plot_tensor(
         C_final.to('cpu'),
-        path=file_dir / 'results' / 'try3_torch_16.png',
+        path=file_dir / 'results' / 'try6_torch_16.png',
         title='Lightfield Tensorring Decomposition - PyTorch (Float16)'
     )
     
@@ -183,7 +188,11 @@ if __name__ == "__main__":
         torch.cuda.current_stream(), 
         grid, 
         contraction, 
-        (A_opt, B_opt, C_opt, opti.config.dim_sizes[3], opti.config.dim_sizes[4], opti.config.dim_sizes[5], opti.config.dim_sizes[6], opti.config.dim_sizes[7])
+        (A_opt, B_opt, C_opt, 
+         opti.config.dim_sizes[0], opti.config.dim_sizes[1], opti.config.dim_sizes[2], 
+         opti.config.dim_sizes[3], opti.config.dim_sizes[4], opti.config.dim_sizes[5], 
+         opti.config.dim_sizes[6], opti.config.dim_sizes[7], opti.config.dim_sizes[8], 
+         opti.config.dim_sizes[9])
     ))
     
     tflops_opt = flops / (t_ms_opt / 1000) / (10**12)
