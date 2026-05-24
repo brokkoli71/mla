4fb0e8dd078f3ca162ac07653f5f794cbec6eb9d
Falko Linke <falkolinke@gmail.com>
2026-05-24 21:13:58 +0200

    Refactor custom_vadd to improve loading and storing of BF16 data

--- assignments/07_assignment/src/custom_vadd.s
+++ assignments/07_assignment/src/custom_vadd.s
@@ -6,31 +6,22 @@
 custom_vadd:
 // Computes C = A + B + B
 // Calling convention: p0 = ptr_in0, p1 = ptr_in1, p2 = ptr_out
-  // TODO: load 128 bytes from ptr_in0 (p0) into a accumulator register (A-slot)
-  vlda bmll0, [p0, #0]
-  // TODO: load 128 bytes from ptr_in1 (p1) into a accumulator register (A-slot)
-  vlda bmll1, [p1, #0]
-  // TODO: insert NOPs to satisfy load latency
-  nop
-  nop
-  nop
+  vlda.conv.fp32.bf16	 cml1, [p1, #0]
+  vlda.conv.fp32.bf16	 cmh1, [p1, #64]	
+  vlda.conv.fp32.bf16	 cml0, [p0, #0]
+  vlda.conv.fp32.bf16	 cmh0, [p0, #64]
   mova	r0, #60
-  // TODO: perform two BF16 elementwise add using the mnemonic found in build/vadd.s (V-slot)
-  vadd.f	dm0, dm0, dm1, r0
-  nop
-  nop
-  nop
+  vadd.f	dm1, dm1, dm1, r0
   nop
   nop
   vadd.f	dm0, dm0, dm1, r0
-  nop
   nop
   // TODO: store result to ptr_out (p2) using a store instruction (S-slot)
   ret lr
-  nop                                 // Delay Slot 5
-  nop                                 // Delay Slot 4
-  nop                                 // Delay Slot 3
-  vst bmll0, [p2, #0]                 // Delay Slot 2
   nop                                 // Delay Slot 1
+  nop                                 // Delay Slot 2
+  nop                                 // Delay Slot 3
+  vst.conv.bf16.fp32	 cml0, [p2, #0] //  Delay Slot 4
+  vst.conv.bf16.fp32	 cmh0, [p2, #64] //  Delay Slot 5
 .Lfunc_end0:
   .size custom_vadd, .Lfunc_end0-custom_vadd
